2fb76e7f323c8be13d4b6bf300753fa76fc8ac42
jeli-t <jelitotomek@gmail.com>
2023-11-07 21:34:23 +0100

    generate simple grid based map

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 # Initialize Pygame
 pygame.init()
@@ -7,13 +8,44 @@ pygame.init()
 # Config
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
+TILL_SIZE = 32
+ROWS = WINDOW_HEIGHT // TILL_SIZE
+COLS = WINDOW_WIDTH // TILL_SIZE
+GRID = [[0 for _ in range(COLS)] for _ in range(ROWS)]
 
 
 class Generator():
     def __init__(self):
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Dungeons Generator")
+        self.new_map(10, 0)
         self.main_loop()
+
+    def new_map(self, path_length, side_rooms):
+        self.screen.fill((255, 255, 255))
+        column = 8
+        row = 8
+        for n in range(path_length):
+            while True:
+                direction = random.randint(0, 3)
+                if direction == 0:
+                    column -= 1
+                elif direction == 1:
+                    row -= 1
+                elif direction == 2:
+                    column += 1
+                elif direction == 3:
+                    row += 1
+
+                if GRID[row][column]:
+                    continue
+                else:
+                    GRID[row][column] = 1
+                    break
+
+            pygame.draw.rect(self.screen, (0,0,0), (column * TILL_SIZE, row * TILL_SIZE, TILL_SIZE, TILL_SIZE))
+        pygame.display.flip()
+
 
     def main_loop(self):
         while True:
@@ -21,10 +53,6 @@ class Generator():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
-            self.screen.fill((255, 255, 255))
-            pygame.display.flip()
-            pygame.time.Clock().tick(60)
 
 
 if __name__ == "__main__":
