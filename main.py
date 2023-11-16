31020649117a8329c03f7844d180ea2ba7606a90
jeli-t <jelitotomek@gmail.com>
2023-11-16 19:10:04 +0100

    add Map class and init the walls

--- main.py
+++ main.py
@@ -10,20 +10,72 @@ pygame.init()
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 960
 TILL_SIZE = 64
+TILL_COLOR = (160, 160, 160)
+WALL_THICKNES = 5
+WALL_COLOR = (90, 90, 90)
 MIN_PATH = 4
 MAX_PATH = 20
 MIN_ROOMS = 0
 MAX_ROOMS = 20
 
 
-class Room(pygame.sprite.Sprite):
+class Room():
     def __init__(self, x, y):
-        super().__init__()
         self.position = Vector2(x, y)
-        self.left_room = False
-        self.top_room = False
-        self.right_room = False
-        self.bottom_room = False
+        self.left_door = True
+        self.top_door = True
+        self.right_door = True
+        self.bottom_door = True
+
+
+class Map():
+    def __init__(self):
+        self.rooms = []
+        self.walls = []
+        self.x_offset = 6 * TILL_SIZE
+        self.y_offset = 6 * TILL_SIZE
+
+    def init_walls(self):
+        self.walls = []
+        for room in self.rooms:
+            if room.left_door:
+                wall_1 = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset, room.position.y * TILL_SIZE + self.y_offset, WALL_THICKNES, TILL_SIZE / 4 + WALL_THICKNES)
+                wall_2 = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset, room.position.y * TILL_SIZE + self.y_offset + 3 * (TILL_SIZE / 4), WALL_THICKNES, TILL_SIZE / 4)
+                self.walls.append(wall_1)
+                self.walls.append(wall_2)
+            else:
+                wall = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset, room.position.y * TILL_SIZE + self.y_offset, WALL_THICKNES, TILL_SIZE)
+                self.walls.append(wall)
+            if room.top_door:
+                wall_1 = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset, room.position.y * TILL_SIZE + self.y_offset, TILL_SIZE / 4 + WALL_THICKNES, WALL_THICKNES)
+                wall_2 = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset + 3 * (TILL_SIZE / 4), room.position.y * TILL_SIZE + self.y_offset, TILL_SIZE / 4, WALL_THICKNES)
+                self.walls.append(wall_1)
+                self.walls.append(wall_2)
+            else:
+                wall = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset, room.position.y * TILL_SIZE + self.y_offset, TILL_SIZE, WALL_THICKNES)
+                self.walls.append(wall)
+            if room.right_door:
+                wall_1 = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset + TILL_SIZE, room.position.y * TILL_SIZE + self.y_offset, WALL_THICKNES, TILL_SIZE / 4 + WALL_THICKNES)
+                wall_2 = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset + TILL_SIZE, room.position.y * TILL_SIZE + self.y_offset + 3 * (TILL_SIZE / 4), WALL_THICKNES, TILL_SIZE / 4)
+                self.walls.append(wall_1)
+                self.walls.append(wall_2)
+            else:
+                wall = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset + TILL_SIZE, room.position.y * TILL_SIZE + self.y_offset, WALL_THICKNES, TILL_SIZE)
+                self.walls.append(wall)
+            if room.bottom_door:
+                wall_1 = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset, room.position.y * TILL_SIZE + self.y_offset + TILL_SIZE, TILL_SIZE / 4 + WALL_THICKNES, WALL_THICKNES)
+                wall_2 = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset + 3 * (TILL_SIZE / 4), room.position.y * TILL_SIZE + self.y_offset + TILL_SIZE, TILL_SIZE / 4 + WALL_THICKNES, WALL_THICKNES)
+                self.walls.append(wall_1)
+                self.walls.append(wall_2)
+            else:
+                wall = pygame.Rect(room.position.x * TILL_SIZE + self.x_offset, room.position.y * TILL_SIZE + self.y_offset + TILL_SIZE, TILL_SIZE + WALL_THICKNES, WALL_THICKNES)
+                self.walls.append(wall)
+
+    def draw(self, screen):
+        for room in self.rooms:
+            pygame.draw.rect(screen, TILL_COLOR, (room.position.x * TILL_SIZE + self.x_offset, room.position.y * TILL_SIZE + self.y_offset, TILL_SIZE, TILL_SIZE))
+        for wall in self.walls:
+            pygame.draw.rect(screen, WALL_COLOR, wall)
 
 
 class Generator():
@@ -31,11 +83,9 @@ class Generator():
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Dungeons Generator")
         self.font = pygame.font.Font(None, 24)
-        self.rooms = []
+        self.map = Map()
         self.path_length = 10
         self.side_rooms = 6
-        self.x_offset = 6 * TILL_SIZE
-        self.y_offset = 6 * TILL_SIZE
         self.slider1_in_use = False
         self.slider2_in_use = False
         self.generate_button = pygame.Rect(20, 130, 100, 30)
@@ -44,11 +94,11 @@ class Generator():
 
 
     def new_map(self, path_length, side_rooms):
-        self.rooms = []
+        self.map.rooms = []
         x_index = 1
         y_index = 1
         previous_room = 'left'
-        self.rooms.append(Room(x_index, y_index))
+        self.map.rooms.append(Room(x_index, y_index))
 
         for n in range(path_length - 1):
             while True:
@@ -71,25 +121,26 @@ class Generator():
                 new_room = Room(x_index, y_index)
 
                 already_exists = False
-                for room in self.rooms:
+                for room in self.map.rooms:
                     if room.position == new_room.position:
                         already_exists = True
                         break
-                
+
                 if already_exists:
                     continue
                 else:
-                    self.rooms.append(new_room)
+                    self.map.rooms.append(new_room)
                     break
+
+        self.map.init_walls()
 
 
     def render(self):
         self.screen.fill((255, 255, 255))
 
         # render map
-        for room in self.rooms:
-            pygame.draw.rect(self.screen, (0,0,0), (room.position.x * TILL_SIZE + self.x_offset, room.position.y * TILL_SIZE + self.y_offset, TILL_SIZE, TILL_SIZE))
-        
+        self.map.draw(self.screen)
+
         # render menu
         pygame.draw.rect(self.screen, (200,200,200), (0, 0, 240, 180))
         text = self.font.render(f"Path length: {self.path_length}", True, (0,0,0))
